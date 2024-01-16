7153de39d6a761d6571c236c246c325410eecd89
Oncu <oohancan@gmail.com>
2024-01-16 12:51:53 +0300

    CoverFlowView added

--- CoverFlow/ContentView.swift
+++ CoverFlow/ContentView.swift
@@ -8,14 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var items: [CoverFlowItem] = [.red, .blue, .green, .yellow, .primary].compactMap {
+        return .init(color: $0)
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                CoverFlowView(itemWidth: 200, items: items) { item in
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(item.color.gradient)
+                }
+            }
+            .navigationTitle("CoverFlow")
         }
-        .padding()
+        .preferredColorScheme(.dark)
     }
 }
 

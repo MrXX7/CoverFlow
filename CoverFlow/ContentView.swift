0dfe9c45b8a9a85faa4a8bfc2b95dd6c853f50be
Oncu <oohancan@gmail.com>
2024-01-16 15:21:15 +0300

    SwiftUI CoverFlowView with Reflection Effect

--- CoverFlow/ContentView.swift
+++ CoverFlow/ContentView.swift
@@ -11,13 +11,44 @@ struct ContentView: View {
     @State private var items: [CoverFlowItem] = [.red, .blue, .green, .yellow, .primary].compactMap {
         return .init(color: $0)
     }
+    
+    @State private var spacing: CGFloat = 0
+    @State private var rotation: CGFloat = .zero
+    @State private var enableReflection: Bool = false
     var body: some View {
         NavigationStack {
             VStack {
-                CoverFlowView(itemWidth: 200, items: items) { item in
+                Spacer(minLength: 0)
+                
+                CoverFlowView(itemWidth: 200,
+                              enableReflection: enableReflection,
+                              spacing: 0,
+                              rotation: 0,
+                              items: items) { item in
                     RoundedRectangle(cornerRadius: 20)
                         .fill(item.color.gradient)
                 }
+                .frame(height: 180)
+                Spacer(minLength: 0)
+                
+                VStack(alignment: .leading, spacing: 10, content: {
+                    Toggle("Toggle Reflection", isOn: $enableReflection)
+                    
+                    Text("Card Spacing")
+                        .font(.caption2)
+                        .foregroundStyle(.gray)
+                    
+                    Slider(value: $spacing, in: -90...20)
+                    
+                    Text("Card Rotation")
+                        .font(.caption2)
+                        .foregroundStyle(.gray)
+                    
+                    Slider(value: $rotation, in: 0...90)
+                })
+                .padding(15)
+                .background(.ultraThinMaterial, in: .rect(cornerRadius: 10))
+                .padding(15)
             }
             .navigationTitle("CoverFlow")
         }
--- CoverFlow/Helpers/CoverFlowView.swift
+++ CoverFlow/Helpers/CoverFlowView.swift
@@ -10,16 +10,28 @@ import SwiftUI
 // Custom View
 struct CoverFlowView<Content: View, Item: RandomAccessCollection>: View where Item.Element: Identifiable {
     var itemWidth: CGFloat
+    var enableReflection: Bool = false
+    var spacing: CGFloat = 0
+    var rotation: Double
     var items: Item
     var content: (Item.Element) -> Content
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHStack(spacing: 0) {
-                ForEach(items) { item in
-                    content(item)
-                        .frame(width: itemWidth)
+        GeometryReader {
+            let size = $0.size
+            ScrollView(.horizontal) {
+                LazyHStack(spacing: 0) {
+                    ForEach(items) { item in
+                        content(item)
+                            .frame(width: itemWidth)
+                            .reflection(enableReflection)
+                    }
                 }
+                .padding(.horizontal, (size.width - itemWidth) / 2)
+                .scrollTargetLayout()
             }
+            .scrollTargetBehavior(.viewAligned)
+            .scrollIndicators(.hidden)
+            .scrollClipDisabled()
         }
     }
 }
@@ -31,6 +43,37 @@ struct CoverFlowItem: Identifiable {
     var color: Color
 }
 
+fileprivate extension View {
+    @ViewBuilder
+    func reflection(_ added: Bool) -> some View {
+        self
+            .overlay {
+                if added {
+                    GeometryReader {
+                        let size = $0.size
+                        
+                        self
+                            .scaleEffect(y: -1)
+                            .mask {
+                                Rectangle()
+                                    .fill(
+                                        .linearGradient(colors: [
+                                            .white,
+                                            .white.opacity(0.7),
+                                            .white.opacity(0.5),
+                                            .white.opacity(0.3),
+                                            .white.opacity(0.1),
+                                            .white.opacity(0),
+                                        ] + Array(repeating: Color.clear, count: 5), startPoint: .top, endPoint: .bottom)
+                                    )
+                            }
+                            .offset(y: size.height + 5)
+                            .opacity(0.5)
+                    }
+                }
+            }
+    }
+}
 #Preview {
     ContentView()
 }

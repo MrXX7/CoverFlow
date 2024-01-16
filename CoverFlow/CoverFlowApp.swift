969a94703a01b7e9bd18494a4d595ca8de296606
Oncu <oohancan@gmail.com>
2024-01-16 12:34:01 +0300

    Initial Commit

---
+++ CoverFlow/CoverFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoverFlowApp.swift
+//  CoverFlow
+//
+//  Created by Oncu Can on 16.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct CoverFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
